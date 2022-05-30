6f730fb878752b28ab3f01563d97650dacd0bd7c
nishekh-e-r <nishekh@higheredlab.com>
2022-05-30 05:27:37 +0000

    fix:ci.yml

--- copy.sh
+++ copy.sh
@@ -4,7 +4,7 @@ npm install
 cd /usr/src/app/
 npm run build
 npm run develop &
-sleep 5
+sleep 10
 cd /usr/src/app/src/plugins/bigbluebutton/
 npx cypress run --browser chrome
 npx cypress run --browser firefox
