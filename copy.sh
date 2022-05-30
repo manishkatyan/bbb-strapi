10a3ae6dfd8eccd8d4ebb60e6a7cd898b9331c98
nishekh-e-r <nishekh@higheredlab.com>
2022-05-30 05:31:12 +0000

    fix: copy.sh

--- copy.sh
+++ copy.sh
@@ -1,10 +1,10 @@
-
+cp -r * /usr/src/app/src/plugins/bigbluebutton/
 cd /usr/src/app/src/plugins/bigbluebutton/
 npm install
 cd /usr/src/app/
 npm run build
 npm run develop &
-sleep 10
+sleep 5
 cd /usr/src/app/src/plugins/bigbluebutton/
 npx cypress run --browser chrome
 npx cypress run --browser firefox
